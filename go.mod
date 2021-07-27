280f1023ca21f0dfa614c6c7eeb81461272c9133
emp1tsu <oneok0813@gmail.com>
2021-07-27 21:01:00 +0900

    blog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/emp1tsu/web-server-golang
 
 go 1.16
+
+require github.com/google/uuid v1.3.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
-	"os"
+
+	"github.com/google/uuid"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	c := getCookie(w, req)
+	tpl.Execute(w, c)
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("session")
+	if err != nil {
+		sID := uuid.New()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
 	}
-	http.ListenAndServe(":"+port, nil)
+
+	return c
 }
