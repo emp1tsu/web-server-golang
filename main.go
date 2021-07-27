0d68752282147d7c935e02eca70f592b3697db76
emp1tsu <oneok0813@gmail.com>
2021-07-27 21:17:57 +0900

    changes

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/google/uuid"
 )
@@ -21,7 +27,38 @@ func main() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 	c := getCookie(w, req)
-	tpl.Execute(w, c)
+
+	if req.Method == http.MethodPost {
+		mf, fh, err := req.FormFile("nf")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer mf.Close()
+
+		ext := strings.Split(fh.Filename, ".")[1]
+		h := sha256.New()
+		io.Copy(h, mf)
+		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(wd, "pics", fname)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+
+		mf.Seek(0, 0)
+		io.Copy(nf, mf)
+
+		c = appendValue(w, c, fname)
+	}
+
+	xs := strings.Split(c.Value, "|")
+	tpl.Execute(w, xs[1:])
 }
 
 func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
@@ -35,5 +72,16 @@ func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
 		http.SetCookie(w, c)
 	}
 
+	return c
+}
+
+func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
+	s := c.Value
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
+	}
+
+	c.Value = s
+	http.SetCookie(w, c)
 	return c
 }
